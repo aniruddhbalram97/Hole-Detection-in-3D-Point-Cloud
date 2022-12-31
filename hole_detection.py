755b1ea7bc217d129fbc0422fe82a125594053be
Aniruddh Balram <abalram1@umd.edu>
2022-12-31 11:51:33 -0500

    Added functions to compute squared distance between two points and Centroid between given points

--- hole_detection.py
+++ hole_detection.py
@@ -18,5 +18,33 @@ class HoleDetection:
         pcdArray = np.asarray(pcd.points)
         return pcd, pcdArray
     
+    def computeSquaredDistance(self, point1, point2):
+        """
+        Function to compute squared distances between two points
+        Args: point1 is an array of form [x y z]
+              point2 is an array of form [x y z]
+        Formula: sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2), 2-Norm of a matrix
+        """
+        x1, y1, z1 = point1[0], point1[1], point1[2]
+        x2, y2, z2 = point2[0], point2[1], point2[2]
+        squaredDistance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+        return squaredDistance
+    
+    def computeCentroid(self, pointArray):
+        """
+        Function to compute centroid for a given array of points
+        Args: pointArray is a 2D array of the form [[x1 y1 z1] [x2 y2 z2] .... [xn, yn, zn]]
+        Formula: Centroid = ((x1 + x2 + ... + xn)/n , (y1 + y2 + ... + yn)/n , (z1 + z2 + ... + zn)/n)
+        """
+        x = 0
+        y = 0
+        z = 0
+        for i in range(0, len(pointArray), 1):
+            x = x + pointArray[i][0]
+            y = y + pointArray[i][1]
+            z = z + pointArray[i][2]
+        centroid = [x / len(pointArray), y / len(pointArray), z / len(pointArray)]
+        return centroid
+    
 pointCloud, pointCloudArray = HoleDetection(os.path.abspath(os.getcwd()) + "/models_3d/dragon_with_hole.obj").readAndConvertObjToPointCloud(5000)
 o3d.visualization.draw_geometries([pointCloud])
